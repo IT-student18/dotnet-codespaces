1b1d5bb94b8118ec7fbe3a33f2a40d9be16ae31b
IT-student18 <firexre@gmail.com>
2025-03-22 21:41:59 +0000

    why dosn't it commit?

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using static System.Console;
+using System.Threading;
+WriteLine("Hello");
+Thread.Sleep(1000);
+WriteLine("Watch this:");
+Thread.Sleep(998);
+char[] a = {'-','\\','|', '/','-','\\','|', '/'};
+int i = 0;
+while (true)
+{
+    if (i > )
+        i = 0;
+    Clear();
+    Write(a[i]);
+    Thread.Sleep(100);
+    i++;
+}
